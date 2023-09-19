ff9827339e137c96df84d88d11239eacfda06144
alevur <alevure@yandex.ru>
2023-09-19 20:20:17 +0400

    Initial commit

---
+++ javadate.js
@@ -0,0 +1,10 @@
+const today = new Date()
+console.log(today);
+
+const test1 = new Date(1992, 4,29);
+console.log(test1);
+
+const age = today.getFullYear()-test1.getFullYear();
+console.log(age)
+
+
